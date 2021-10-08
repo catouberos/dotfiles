227f53a088e01082ce1b2eb1b353f777cdbd1047
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2021-10-08 09:49:22 +0700

    Fixed san's start script

--- .config/polybar/themes/san/run.sh
+++ .config/polybar/themes/san/run.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -r bar --config=config &
+polybar -r bar --config=$HOME/.config/polybar/themes/san/config.ini &
 
 echo "Polybar launched..."
